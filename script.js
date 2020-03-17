9ec920e1b20fab10eab2cb688b056ef0ec5032c7
PadmaGnanapriya <padmaisuru@gmail.com>
2020-03-17 10:14:08 +0530

    make increament one by one cats with button press.

---
+++ script.js
@@ -0,0 +1,6 @@
+function genarateCat(){
+    var image=document.createElement('img');
+    var div=document.getElementById('flex-cat-gen');
+    image.src="https://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+    div.appendChild(image);
+}
